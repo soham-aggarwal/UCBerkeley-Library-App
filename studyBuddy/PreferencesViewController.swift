f8efe4e6b263b2c95b2865d3cbe72e6d5bb8a34d
Adhyyan Narang <adhyyan@adhyyans-mbp.attlocal.net>
2017-05-02 08:22:05 -0700

    Added the file

---
+++ studyBuddy/PreferencesViewController.swift
@@ -0,0 +1,59 @@
+//  ViewController.swift
+//  studyBuddy
+//
+//  Created by Adhyyan Narang on 4/13/17.
+//  Copyright © 2017 Adhyyan Narang. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class PreferencesViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBOutlet weak var moffitt: UITextField!
+    
+    @IBOutlet weak var doe: UITextField!
+    
+    @IBOutlet weak var kresge: UITextField!
+    
+    @IBOutlet weak var eshleman: UITextField!
+    
+    @IBOutlet weak var environmental: UITextField!
+    
+    @IBOutlet weak var mlkb: UITextField!
+    
+    @IBOutlet weak var mlku: UITextField!
+    
+    @IBAction func finishSigningUp(_ sender: Any) {
+        let parameters: [String:Any] = [
+            "moffitt": moffitt.text ?? "",
+            "doe": doe.text ?? "",
+            "kresge": kresge.text ?? "",
+            "eshleman": eshleman.text ?? "",
+            "environmental library" : environmental.text ?? "",
+            "mlkb": mlkb.text ?? "",
+            "mlku": mlku.text ?? ""
+        ]
+        
+        Alamofire.request(.PUT, "https://library-adhyyan.herokuapp.com/api/users/put", parameters:parameters)
+            .response { (request, response, data, error) in
+                println(request)
+                println(response)
+                println(error)
+        }
+    }
+    
+    
+    
+    
+}
+
