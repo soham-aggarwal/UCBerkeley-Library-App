877898a8dada9d6745547fac063a26ef6dad2d81
Adhyyan Narang <adhyyan@adhyyans-mbp.attlocal.net>
2017-05-02 07:17:59 -0700

    Algorithm stuff

--- studyBuddy/Libraries.swift
+++ studyBuddy/Libraries.swift
@@ -1,4 +1,3 @@
-//
 //  Libraries.swift
 //  studyBuddy
 //
@@ -7,3 +6,72 @@
 //
 
 import Foundation
+import Alamofire
+import SwiftyJSON
+
+class LibrariesModel {
+    
+    struct Library {
+        var name: String?
+        var preference: Int?
+        var distance: Int?
+        var percentageFull: Int?
+        var totalScore: Int?
+        
+        init(name:String) {
+            self.name = name
+        }
+    }
+    
+    var allLibraryOptions: [Library] = []
+    
+    //Now we need to fill up the allLibraryOptions variable.
+    
+    /* Make the API call to my API to get the names inside the initializer. Then inside the completion handler, call my API to getPreferences(). Then, inside the completion handler, make a function call to getDistance(). Inside the completion handler for getDistance(), make a function call to getPercentage(). Now all the information has been obtained and I can safely perform my algorithm.
+     */
+    
+    init() {
+        Alamofire.request("https://library-adhyyan.herokuapp.com/api/libraries", method: .get).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                let number = json.arrayValue.count
+                for i in 0...number {
+                    let path: [JSONSubscriptType] = [i, "name"]
+                    let libraryOptionName = Library(
+                        name: json[path].stringValue
+                    )
+                    self.allLibraryOptions.append(libraryOptionName)
+                }
+                self.getPreferences()
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    func getPreferences() -> Void {
+        let reqUrl: String = "https://library-adhyyan.herokuapp.com/api/users/" + id
+        Alamofire.request(reqUrl, method: .get).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                let number = json.arrayValue.count
+                for i in 0...number {
+                    let path: [JSONSubscriptType] = ["preferences", self.allLibraryOptions[i].name!]
+                    let prefNumber = json[path].intValue
+                    self.allLibraryOptions[i].preference = prefNumber
+                    self.getDistances()
+                }
+                self.getPreferences()
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    func getDistances() -> Void {
+        
+    }
+    
+}
